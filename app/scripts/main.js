f102d47557499ced095ceb5b180645ef1d3d4fd2
Sonny King <Sonny.King@frogdesign.com>
2013-01-24 22:40:46 -0800

    added iscroll, fixed scss partial names and ref cdn zepto to fix the define not defined error

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,9 @@ window.mozapps = {
   }
 };
 
-document.ready(function(){
+$(document).ready(function(){
   mozapps.init();
+  // temp will move to view render
+  var myScroll = new iScroll('iscroll', { hScroll: true, vScroll: false, hScrollbar: false, vScrollbar: false });
+
 });
